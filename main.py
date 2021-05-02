f0e47d3797d2b48dbb1a39581c6c885455a47cd0
uribo <ur1b0.dev@gmail.com>
2021-05-03 04:24:58 +0900

    エラーログのファイルを分けた

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ f = open("./config.json", "r")
 conf = json.loads(f.read())
 f.close()
 
-
-readSer = serial.Serial(conf["serial_port"], conf["serial_rate"], timeout=3)
-
 a = ambient.Ambient(33580, conf["ambient_key_write"])
 
 while 1:
     filename = datetime.datetime.now().strftime("%Y-%m-%d")
     timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    f = open(conf["logdir"] + "/" + filename + ".csv", mode="a")
+    readSer = serial.Serial(conf["serial_port"],
+                            conf["serial_rate"], timeout=3)
     raw = readSer.readline().decode().replace('\n', '')
+    readSer.close()
     line = raw.split(";")
     if line[0].split("=")[1].isdecimal():
         temp = int(line[0].split("=")[1]) / 100
@@ -27,10 +26,12 @@ while 1:
         res = a.send({"d1": temp, "d2": humid}, timeout=60)
         row = str(timestamp) + "," + str(temp) + \
             "," + str(humid) + "," + str(raw)
+        f = open(conf["logdir"] + "/" + filename + ".csv", mode="a")
+        f.write(row + "\n")
+        f.close()
     else:
         row = str(timestamp) + "," + "0.0,0.0," + "," + str(raw)
-    f.write(row + "\n")
-    f.close()
+        f = open(conf["logdir"] + "/err_" + filename + ".csv", mode="a")
+        f.write(row + "\n")
+        f.close()
     time.sleep(conf["interval"])
-
-readSer.close()
