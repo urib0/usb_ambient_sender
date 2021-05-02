35322d80554b00a15fba7dfc18f497b241ab14d1
uribo <ur1b0.dev@gmail.com>
2021-05-02 16:17:04 +0100

    jsonからポートやトークンをとってくるようにした

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import serial
 import ambient
 import time
+import json
 
 TIME = 300
 
-readSer = serial.Serial('hoge', 9600, timeout=3)
+f = open("./config.json", "r")
+conf = json.loads(f.read())
+f.close()
 
-a = ambient.Ambient(33580, "hoge")
+
+readSer = serial.Serial(conf["serial_port"], conf["serial_rate"], timeout=3)
+
+a = ambient.Ambient(33580, conf["ambient_key_write"])
 
 while 1:
     raw = readSer.readline().decode().replace('\n', '')
@@ -16,9 +22,8 @@ while 1:
         humid = int(line[1].split("=")[1]) / 100
         res = a.send({"d1": temp, "d2": humid}, timeout=60)
 #        print( str(res) + "    " + str(temp) + "," + str(humid) )
-    else:
-        #        print(time.strftime("%Y/%m/%d/%H:%M:%S") + "," + str(raw))
-
+#    else:
+#        print(time.strftime("%Y/%m/%d/%H:%M:%S") + "," + str(raw))
     time.sleep(TIME)
 
 readSer.close()
