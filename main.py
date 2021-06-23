310501f4b15ba88857b7326f15c0f20d592bc1df
uribo <ur1b0.dev@gmail.com>
2021-06-23 20:18:38 +0900

    複数のUSBシリーズを送信できるようにした

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# python3.7で動作確認済み
 
 import serial
 import ambient
@@ -6,31 +7,64 @@ import time
 import json
 import datetime
 import random
+import os
 
+DEBUG = True
+
+
+def logging(name, data):
+    filename = name + "_" + datetime.datetime.now().strftime("%Y-%m-%d") + ".csv"
+    timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    write_str = timestamp + "," + data
+    path = "./" + conf["logdir"] + "/" + name + "/"
+
+    if DEBUG:
+        print(write_str)
+
+    os.makedirs(path, exist_ok=True)
+    f = open(path + filename, mode="a")
+    f.write(write_str + "\n")
+    f.close()
+
+
+def conv(data):
+    if data.split("=")[0] in {"temp", "hum"}:
+        return int(data.split("=")[1]) / 100
+    else:
+        return int(data.split("=")[1])
+
+
+# 設定値読み込み
 f = open("./config.json", "r")
 conf = json.loads(f.read())
 f.close()
 
-a = ambient.Ambient(conf["ambient_channel"], conf["ambient_key_write"])
+am = ambient.Ambient(conf["ambient_channel"], conf["ambient_key_write"])
 
 while 1:
-    filename = datetime.datetime.now().strftime("%Y-%m-%d")
-    timestamp = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    readSer = serial.Serial(conf["serial_port"],
-                            conf["serial_rate"], timeout=3)
-    raw = readSer.readline().decode().replace('\n', '')
-    readSer.close()
-    line = raw.split(";")
+    data_dic = {}
+    for i in conf["devices"]:
+        # シリアル読み込み
+        readSer = serial.Serial(i["serial_port"], i["serial_rate"], timeout=3)
+        raw = readSer.readline().decode().replace('\n', '')
+        readSer.close()
 
-    temp = int(line[0].split("=")[1]) / 100
-    humid = int(line[1].split("=")[1]) / 100
-    res = a.send({"d1": temp, "d2": humid}, timeout=60)
+        # データ整形
+        data_arr = []
+        for j in range(len(i["sensors"])):
+            d = conv(raw.split(";")[j])
+            data_dic[i["sensors"][j]] = d
+            data_arr.append(str(d))
+
+        # ログファイル出力
+        data_arr.append(raw)
+        logging(i["sensor_name"], ",".join(data_arr))
+
+    # ambient送信処理
+    res = am.send(data_dic, timeout=60)
     if 200 != res.status_code:
         time.sleep(random.randint(1, 10))
-        res = a.send({"d1": temp, "d2": humid}, timeout=60)
-    row = str(timestamp) + "," + str(temp) + \
-        "," + str(humid) + "," + str(raw) + "," + str(res.status_code)
-    f = open(conf["logdir"] + "/" + filename + ".csv", mode="a")
-    f.write(row + "\n")
-    f.close()
+        res = am.send(data_dic, timeout=60)
+
+    # 送信周期待ち
     time.sleep(conf["interval"])
